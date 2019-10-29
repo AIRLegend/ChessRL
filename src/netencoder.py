b8c6667fddcaab72a90d5a15288e3c5f581f4400
AIRLegend <alvaroibrain@hotmail.com>
2019-10-29 17:03:26 +0100

    Changed board representations to channels last style and fixed a bug in get_pieces_one_hot. Also added _ to the functions which are not suposed to be used externally.

--- src/netencoder.py
+++ src/netencoder.py
@@ -1,13 +1,13 @@
 """
 Module with all the necessary stuff to encode/decode game states for using them
-with a neural network
+with a neural network.
 """
 
 import numpy as np
 import chess
 
 
-def get_pieces_one_hot(board, color=False):
+def _get_pieces_one_hot(board, color=False):
     """ Returns a 3D-matrix representation of the pieces for one color.
     The matrix ins constructed as follows:
         8x8 (Chess board) x 7 possible pieces (including empty). = 448.
@@ -18,55 +18,69 @@ def get_pieces_one_hot(board, color=False):
     Returns:
         mask: numpy array, 3D matrix with the pieces of the player.
     """
-    mask = np.zeros((7, 8, 8))
+    mask = np.zeros((8, 8, 7))
     for i in list(chess.PIECE_TYPES):
-        mask[i, :, :] = np.array(board.pieces(i, color).mirror()
+        mask[:, :, i] = np.array(board.pieces(i, color).mirror()
                                  .tolist()).reshape(8, 8)
-        # Encode blank positions
-        mask[0, :, :] = (~np.array(mask.sum(axis=0), dtype=bool)).astype(int)
-        return mask
+    # Encode blank positions
+    mask[:, :, 0] = (~np.array(mask.sum(axis=-1), dtype=bool)).astype(int)
+    return mask
 
 
-def get_current_game_state(board):
+def _get_current_game_state(board):
     """ This method returns the matrix representation of a game turn
     (positions of the pieces of the two colors)
 
+    Parameters:
+        board: Python-Chess board
     Returns:
         current: numpy array. 3D Matrix with dimensions 14x8x8.
         """
+    return np.concatenate((_get_pieces_one_hot(board, color=False),
+                           _get_pieces_one_hot(board, color=True)),
+                          axis=-1)
 
-    return np.concatenate((get_pieces_one_hot(board, color=False),
-                           get_pieces_one_hot(board, color=True)),
-                          axis=0)
 
+def _get_game_history(board, T=8):
+    """ Returns the matrix representation of a board history. If a game has no
+    history, those turns will be considered null (represented as 0's matrices).
 
-def get_game_history(board, T=8):
+    Parameters:
+        board: Python-Chess board (with or without moves)
+        T: number of backwards steps to represent. (default 8 as in AlphaZero).
+    Returns:
+        history: NumPy array of dimensions 8x8x(14*T). Note that this history
+        does not include the current game state, only the previous ones.
+    """
     board_copy = board.copy()
-    history = np.zeros((14 * T, 8, 8))
+    history = np.zeros((8, 8, 14 * T))
 
     for i in range(T):
         try:
             board_copy.pop()
         except IndexError:
             break
-        history[i * 14: (i + 1) * 14, :, :] =\
-            get_current_game_state(board_copy)
+        history[:, :, i * 14: (i + 1) * 14] =\
+            _get_current_game_state(board_copy)
 
     return history
 
 
-def get_game_state(board):
+def get_game_state(game):
     """ This method returns the matrix representation of a game with its
     history of moves.
 
+    Parameters:
+        game: Game. Game state.
     Returns:
-        current: numpy array. 3D Matrix with dimensions (14T)x8x8. Where T
-        corresponds to the number of backward turns in time. (And the 14 is
-        the current representation of the two players) """
+        current: numpy array. 3D Matrix with dimensions 8x8x[14(T+1)]. Where T
+        corresponds to the number of backward turns in time.
+    """
 
-    current = get_current_game_state(board)
-    history = get_game_history(board)
-    current = np.concatenate((current, history), axis=0)
+    board = game.board
+    current = _get_current_game_state(board)
+    history = _get_game_history(board)
+    current = np.concatenate((current, history), axis=-1)
     return current
 
 
