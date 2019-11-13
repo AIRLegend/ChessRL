275ce48b7819c005b1b21e40487b73f1274b3d98
AIRLegend <alvaroibrain@hotmail.com>
2019-11-13 15:33:53 +0100

    Agent now returns best_move as a string. For some reason It returned chess.Movement type, which was wrong.

--- src/chessrl/agent.py
+++ src/chessrl/agent.py
@@ -1,9 +1,10 @@
-import mctree
 import numpy as np
+
+import mctree
 import netencoder
 
-from .player import Player
-from .model import ChessModel
+from player import Player
+from model import ChessModel
 
 
 class Agent(Player):
@@ -15,10 +16,10 @@ class Agent(Player):
         uci_dict: dict. Dictionary with mappings 'uci'-> int. It's used
         to predict the policy only over the legal movements.
     """
-    def __init__(self, color):
+    def __init__(self, color, weights=None):
         super().__init__(color)
 
-        self.model = ChessModel(compile_model=True)
+        self.model = ChessModel(compile_model=True, weights=weights)
         self.move_encodings = netencoder.get_uci_labels()
         self.uci_dict = {u: i for i, u in enumerate(self.move_encodings)}
 
@@ -32,15 +33,18 @@ class Agent(Player):
             game: Game. Current game before the move of this agent is made.
             real_game: Whether to use MCTS or only the neural network (self
             play vs playing in a real environment).
+
+        Returns:
+            str. UCI encoded movement.
         """
-        best_move = None
+        best_move = '00000'  # Null move
         if real_game:
             policy = self.predict_policy(game)
             best_move = game.get_legal_moves()[np.argmax(policy)]
         else:
             if game.get_result() is None:
                 self.current_tree = mctree.Tree(game)
-                best_move = self.current_tree.select(self, 900, verbose=False)
+                best_move = str(self.current_tree.select(self, max_iters=900))
         return best_move
 
     def predict_outcome(self, game:'Game') -> float:  # noqa: E0602, F821
@@ -57,7 +61,7 @@ class Agent(Player):
             policy = [policy[self.uci_dict[x]] for x in legal_moves]
         return policy
 
-    def train(self, dataset: netencoder.DatasetGame, epochs=2, logdir=None):
+    def train(self, dataset: 'DatasetGame', epochs=2, logdir=None):  # noqa E0602
         """ Trains the model using previous recorded games """
         if len(dataset) > 0:
             datagen = netencoder.DataGameSequence(dataset, batch_size=1)
