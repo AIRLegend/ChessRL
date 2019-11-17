f96ef46216ebc582f46d746c707d3b5285cc1421
AIRLegend <alvaroibrain@hotmail.com>
2019-11-17 14:25:33 +0100

    Created a method to display the MCT on the browser, which is more fluid that the previous method using networkx.

--- src/chessrl/lib/treeviewer.py
+++ src/chessrl/lib/treeviewer.py
@@ -4,6 +4,9 @@ import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 from matplotlib import pyplot as plt
 
+import tempfile
+import webbrowser
+import os
 
 def draw_tree(tree: Tree):
     g = nx.DiGraph()
@@ -36,3 +39,32 @@ def __add_node(graph: nx.DiGraph, node: Node, labels_dict: dict):
         # labels_dict[hash(c)] = {'val': c.value}
         labels_dict[hash(c)] = '' + "{0:.2g}".format(c.value)
         __add_node(graph, c, labels_dict)
+
+
+def draw_tree_html(tree: Tree):
+    """ Plots a tree on the browser """
+    template_path = path = os.path.abspath(__file__ + "/../") + \
+        "/static/template.html"
+
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    path = tmp.name + '.html'
+
+    # load template
+    with open(template_path, 'r') as file:
+        filestr = file.read()
+
+    tree_json = __json(tree.root)
+
+    filestr = filestr.replace("{_DATA_}", str(tree_json))
+
+    with open(path, 'w') as file:
+        file.write(filestr)
+
+    webbrowser.open('file://{}'.format(path))
+
+
+def __json(node):
+    node_json = {}
+    node_json['text'] = {'name': f'V: {node.get_value()}'}
+    node_json['children'] = [__json(n) for n in node.children]
+    return node_json
